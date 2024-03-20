8a4e97f5eddc8721a9827e60e3b1ff019a24debb
gencaypolat <gencaypolat@gmail.com>
2024-03-20 14:44:33 +0300

    Differences - Transient, Singleton, Scoped

--- DIExample/Controllers/HomeController.cs
+++ DIExample/Controllers/HomeController.cs
@@ -7,18 +7,29 @@ namespace DIExample.Controllers
 {
     public class HomeController : Controller
     {
-        //private readonly ICitiesService _citiesService;
-        //// constructor
-        //public HomeController(ICitiesService citiesService)
-        //{
-        //    // create object of service class
-        //    _citiesService = citiesService;  //new CitiesService();
-        //}
-        
-        [Route("/")]
-        public IActionResult Index([FromServices]ICitiesService _citiesService)
+        private readonly ICitiesService _citiesService1;
+        private readonly ICitiesService _citiesService2;
+        private readonly ICitiesService _citiesService3;
+
+        //constructor
+        public HomeController(ICitiesService citiesService1, ICitiesService citiesService2, ICitiesService citiesService3)
         {
-            List<string> cities = _citiesService.GetCities();
+            _citiesService1 = citiesService1;
+            _citiesService2 = citiesService2;
+            _citiesService3 = citiesService3;
+        }
+
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            List<string> cities = _citiesService1.GetCities();
+            ViewBag.InstanceId_CitiesService_1 = _citiesService1.ServiceInstanceId;
+
+            ViewBag.InstanceId_CitiesService_2 = _citiesService2.ServiceInstanceId;
+
+            ViewBag.InstanceId_CitiesService_3 = _citiesService3.ServiceInstanceId;
+
             return View(cities);
         }
     }
